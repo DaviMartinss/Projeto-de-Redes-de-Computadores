6e3dc978e7a63ee98295a95d4865b2552680cc8a
Davi Martins <davimartins.dgcm@gmail.com>
2022-01-15 09:38:38 -0300

    Enviando mensagem para o Servidor

--- src/chat/blocking/ChatClient.java
+++ src/chat/blocking/ChatClient.java
@@ -5,8 +5,12 @@
  */
 package chat.blocking;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 /**
@@ -16,9 +20,28 @@ import java.util.logging.Logger;
 public class ChatClient {
     private final String SERVER_ADDRESS = "127.0.0.1";
     private Socket clientSocket;
+    private Scanner scanner;
+    private PrintWriter out;
+    
+    public ChatClient(){
+        scanner = new Scanner(System.in, "Cp1252");
+    }
     
     public void start() throws IOException{
         clientSocket = new Socket(SERVER_ADDRESS, ChatServer.PORT);
+        this.out = new PrintWriter(clientSocket.getOutputStream(), true); // envia mensagem para o servidor
+        System.out.println("Cliente conectado ao servidor em "+ SERVER_ADDRESS + ":" +ChatServer.PORT);
+        messageLoop();
+    }
+    
+    private void messageLoop() throws IOException{
+        String msg ;
+        do{
+            System.out.print("Digite uma mensagem (ou sair para finalizar): ");
+            msg = scanner.nextLine();
+            
+            out.println(msg); // envia a mensagem
+        }while (!msg.equalsIgnoreCase("sair"));
     }
     
     public static void main(String[] args) {
@@ -29,5 +52,6 @@ public class ChatClient {
         } catch (IOException ex) {
             System.out.println("Erro ao iniciar o clientt: "+ex.getMessage());
         }
+        System.out.println("Cliente Finalizado");
     }
 }
--- src/chat/blocking/ChatServer.java
+++ src/chat/blocking/ChatServer.java
@@ -5,7 +5,9 @@
  */
 package chat.blocking;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -26,9 +28,14 @@ public class ChatServer {
     private void clientConnectionLoop() throws IOException{
         while (true) {            
            Socket clientSocket = serverSockert.accept();
-            System.err.println("Client "+clientSocket.getReuseAddress());
+           System.out.println("Client "+clientSocket.getRemoteSocketAddress() +" conectou");
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
+            String msg = in.readLine();
+            System.out.println("Messagem recebida do cliente = " +clientSocket.getRemoteSocketAddress()
+                + ":" +msg);
         }
     }
+
     public static void main(String[] args) {
         try{
             ChatServer server = new ChatServer();
@@ -36,6 +43,6 @@ public class ChatServer {
         }catch(IOException ex){
             System.out.println("Erro ao iniciar o servidor: "+ex.getMessage());
         }
-        System.err.println("Servidor Finalizado");
+        System.out.println("Servidor Finalizado");
     }
 }
